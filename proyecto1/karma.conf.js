d190f5d067719e70d087ec25ce911e6b229119de
Lewis <lewisv4re@gmail.com>
2022-11-10 15:36:44 -0500

    primer envio

--- proyecto1/karma.conf.js
+++ proyecto1/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/proyecto1'),
+      dir: require('path').join(__dirname, './coverage/proyect'),
       subdir: '.',
       reporters: [
         { type: 'html' },
