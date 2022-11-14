e3f9edc24d8fdcc3edddc24f1505ce2521cb8acb
Lewis <lewisv4re@gmail.com>
2022-11-14 10:42:26 -0500

    implementacion de alertas y validacion fake del login

--- proyecto1/src/app/components/login/login.component.ts
+++ proyecto1/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,15 +9,45 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  logo = 'http://assets.stickpng.com/images/5847ea22cef1014c0b5e4833.png'
+  logo = '../../assets/img/clave.png'
+  form: FormGroup;
+  loading = false;
+  enterprise: any;
   
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private fb: FormBuilder,
+    private _snackBar: MatSnackBar) {
+      this.form = this.fb.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required],
+      });
+     }
 
   ngOnInit(): void {
   }
 
   ingresar() {
-    this.router.navigateByUrl('/dashboard');
+    if(this.form.value.username == 'lewis' && this.form.value.password == 'lewis'){
+      this.router.navigateByUrl('/dashboard');
+    }else{
+      this.error();
+    }
+
+  }
+
+  error() {
+    this._snackBar.open('Usuario o contraseña invalido', '', {
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
+  }
+
+  fakeLoading() {
+    this.loading = true;
+    setTimeout(() => {
+      this.router.navigate(['dashboard']);
+    }, 1500);
   }
 
 }
